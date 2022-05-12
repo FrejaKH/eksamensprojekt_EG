5f335bc2ae624983d1b0f68c6473501ee59b4023
Freja <freja.hyldgaard@gmail.com>
2022-05-12 13:02:31 +0200

    my first views finished

--- models/db.js
+++ models/db.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2/promise");
 // https://www.npmjs.com/package/mysql2#using-connection-pools - Using connection pools
 module.exports = mysql.createPool({
   host: "127.0.0.1",
-  user: "reneseebach",
-  password: "hvu2375",
+  user: "root",
+  password: "root",
   database: "dbbyggemarked",
 });
 
