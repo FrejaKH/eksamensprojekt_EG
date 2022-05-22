6c4992e68061ef2f456dc8110f72d0407ea68ab1
MortenRUD <morten@mortenrudjensen.dk>
2022-05-22 18:08:36 +0200

    added arrow to popup with div rotate and gradient

--- public/javascripts/popup.js
+++ public/javascripts/popup.js
@@ -1,20 +1,24 @@
 let popup_btn = document.getElementById("popup_btn");
 let popup_btn_close = document.getElementById("popup_header_btn");
 let popup = document.getElementById("popup");
+let popup_arrow = document.getElementById("popup_arrow");
 
 function open_popup() {
     if (!popup.classList.contains("show")) {
         popup.classList.add("show");
+        popup_arrow.classList.add("show");
     }
     else return;
 }
 
 function toggle_popup() {
     popup.classList.toggle("show");
+    popup_arrow.classList.toggle("show");
 }
 
 function close_popup(){
         popup.classList.remove("show"); 
+        popup_arrow.classList.remove("show"); 
 }
 
 popup_btn.addEventListener("click", toggle_popup);
